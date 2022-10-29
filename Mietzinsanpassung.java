eee0f6dbaa87d96317034e0d9515d5c92cc89821
Jacques Mock Schindler <jacques.mock@kbw.ch>
2022-10-29 18:23:10 +0200

    Create Class Scaffold

---
+++ Mietzinsanpassung.java
@@ -0,0 +1,25 @@
+class Mietzinsanpassung{
+    private double rent;
+    private double mortgageInterestRateOld;
+    private double mortgageInterestRateNew;
+    private double maxRentChange;
+
+    public int checkInterestRate(double mortgageInteresRateOld) {
+        //TODO: Methode implementieren
+        return 0;
+    }
+
+    public double calculateMaxRentChange(double rent, int interestClass) {
+        //TODO: Methode implementieren
+        return 0;
+    }
+
+    public void print(double maxRentChange) {
+        //TODO: Methode implementieren
+    }
+
+    public static void main(String[] args) {
+        //TODO: Input abfragen und den Methoden übergeben
+    }
+
+}
